19d19086801736b59146171fd710423e557118fd
Matěj Kohútek <matej.kohutek@gmail.com>
2019-03-09 01:58:32 +0100

    Create Interfaces

---
+++ Diary/IDiaryConnector.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diary
+{
+    interface IDiaryConnector
+    {
+        IDiaryItem Create(IDiaryItem item);
+
+        Dictionary<Guid, IDiaryItem> Read(IDiaryItem item);
+
+        IDiaryItem Update(IDiaryItem item);
+
+        bool Delete(IDiaryItem item);
+    }
+}
---
+++ Diary/IDiaryItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diary
+{
+    interface IDiaryItem
+    {
+        Guid guid { get; set; }
+        string title { get; set; }
+        string description { get; set; }
+        DateTime dateTimeStart { get; set; }
+        DateTime dateTimeEnd { get; set; }
+        string[] labels { get; set; }
+    }
+}
